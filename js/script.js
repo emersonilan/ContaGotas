85cf5722dad153b9a45b51fc00e9413fb2ffffc9
emersonilan <emerson.ilan@aluno.senac.rio>
2026-01-22 21:46:52 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-/** Javascripts do site
- * 
+/**
+ * JavaScripts do Site
  */
 
-
-// confirm('tem certeza que voce é voce?')
-
-<<<<<<< HEAD
 console.log('Estou funcionando!');
-=======
-console.log('Estou funcionando!');
-
->>>>>>> 089fe9af0081197e9bdff9d4f1cb6116fc77d54a
